a113cdb235f40f807e045456ab5f48f9f0c5d78a
Katerina <kslezakova12@gmail.com>
2021-08-01 09:08:13 +0200

    added link to my linkedin profile to my project in Path.js component

--- src/Path.js
+++ src/Path.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Path() {
   return (
     <small>
-      This project was coded by Katerina Slezakova and is <a href="https://github.com/KaterinaSlezakova/react-weather-app" target="_blank">
+      This project was coded by <a href="https://www.linkedin.com/in/kslezakova" target="_blank">Katerina Slezakova</a> and is <a href="https://github.com/KaterinaSlezakova/react-weather-app" target="_blank">
         open-sourced on GitHub
       </a> and <a href="https://practical-torvalds-69059b.netlify.app"target="_blank">
         hosted on Netlify.
