320d3dbccea179485732a26570b2ce0b6210f408
Katerina <kslezakova12@gmail.com>
2021-08-01 09:51:07 +0200

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Form() {
 return (
-<form id="search-form" className="mb-4">
+<form className="mb-4">
         <div className="row">
           <div className="col-8">
             <input
@@ -10,14 +10,13 @@ return (
               placeholder="Type a city.."
               className="form-control"
               autoComplete="off"
-              id="city-input"
               />
         </div>
           <div className="col-1">
-              <button type="submit" className="btn btn-primary search"><i className="fas fa-search"></i></button>
+              <button type="submit" className="btn btn-primary"><i className="fas fa-search"></i></button>
           </div>
           <div className="col-1">
-              <button type="button" className="btn btn-warning" id ="current-position-button"><i className="fas fa-map-marker-alt"></i></button>
+              <button type="button" className="btn btn-warning"><i className="fas fa-map-marker-alt"></i></button>
           </div>
         </div> 
       </form>
