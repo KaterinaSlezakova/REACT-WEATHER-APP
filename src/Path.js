66634556d3ea274d11e2ac55670ddb727c352bb6
Katerina <kslezakova12@gmail.com>
2021-08-01 08:56:16 +0200

    deleted unnecessary component + updated links in Path.js component

--- src/Path.js
+++ src/Path.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Path() {
   return (
     <small>
-      This project was coded by Katerina Slezakova and is <a href="https://github.com/KaterinaSlezakova/react-weather-app">
+      This project was coded by Katerina Slezakova and is <a href="https://github.com/KaterinaSlezakova/react-weather-app" target="_blank">
         open-sourced on GitHub
-      </a> and <a href="https://practical-torvalds-69059b.netlify.app">
+      </a> and <a href="https://practical-torvalds-69059b.netlify.app"target="_blank">
         hosted on Netlify.
       </a>
     </small>
