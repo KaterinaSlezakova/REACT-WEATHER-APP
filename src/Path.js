0a71fcb0f3a0c229d575aa211cf6ca76f1f4eea0
Katerina <kslezakova12@gmail.com>
2021-08-05 20:58:35 +0200

    added rel="noreferrer" to fix the issue in deploying site in Netlify

--- src/Path.js
+++ src/Path.js
@@ -3,9 +3,28 @@ import React from "react";
 export default function Path() {
   return (
     <small>
-      This project was coded by <a href="https://www.linkedin.com/in/kslezakova" target="_blank">Katerina Slezakova</a> and is <a href="https://github.com/KaterinaSlezakova/react-weather-app" target="_blank">
+      This project was coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/kslezakova"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Katerina Slezakova
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://github.com/KaterinaSlezakova/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
         open-sourced on GitHub
-      </a> and <a href="https://practical-torvalds-69059b.netlify.app"target="_blank">
+      </a>{" "}
+      and{" "}
+      <a
+        href="https://practical-torvalds-69059b.netlify.app"
+        target="_blank"
+        rel="noreferrer"
+      >
         hosted on Netlify.
       </a>
     </small>
