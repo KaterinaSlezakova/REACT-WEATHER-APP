232b8b01c8e2eff838242c259f6bc91c2608c7cc
Katerina <kslezakova12@gmail.com>
2021-08-12 18:16:23 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
-import Axios from "axios";
+import axios from "axios";
 
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-  const [loaded, setLoaded] = useState(false);
-  const [forecast, setForecast] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -30,12 +30,12 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "e23112c75ac1ee62d79ac9963082cc55";
+    let apiKey = "1e62bb63429199d36c95139a4a56613e";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}units=metric`;
-
-    Axios.get(apiUrl).then(handleResponse);
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
     
     return null;
   }
