f9873db3fdc23bcbe188f813984b54d35c96b18b
Katerina <kslezakova12@gmail.com>
2021-09-30 23:07:04 +0200

    Update CurrentPosition.js

--- src/CurrentPosition.js
+++ src/CurrentPosition.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 export default function CurrentPosition() {
+function displayWeather(response){
+console.log(response);
+}
+
   function getLocation(event) {
     event.preventDefault();
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(searchPosition);
   }
-  function showPosition(position) {
-    console.log(position.coords.longitude);
-    console.log(position.coords.latitude);
+  function searchPosition(position) {
+    let longitude= position.coords.longitude;
+    let latitude= position.coords.latitude;
+    let apiKey = "2fe472309e4eca35c3771be7be704291";
+    let units= "metric"
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`
+    axios.get(apiUrl).then(displayWeather);
   }
   return (
     <div className="col-1">
