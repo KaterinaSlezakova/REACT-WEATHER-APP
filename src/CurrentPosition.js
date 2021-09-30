b874317a46238086b6d2bbe716a106f88b53c351
Katerina <kslezakova12@gmail.com>
2021-09-30 23:26:01 +0200

    little updates in app

--- src/CurrentPosition.js
+++ src/CurrentPosition.js
@@ -5,12 +5,7 @@ export default function CurrentPosition() {
 function displayWeather(response){
 console.log(response);
 }
-
-  function getLocation(event) {
-    event.preventDefault();
-    navigator.geolocation.getCurrentPosition(searchPosition);
-  }
-  function searchPosition(position) {
+function searchPosition(position) {
     let longitude= position.coords.longitude;
     let latitude= position.coords.latitude;
     let apiKey = "2fe472309e4eca35c3771be7be704291";
@@ -18,10 +13,15 @@ console.log(response);
     let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`
     axios.get(apiUrl).then(displayWeather);
   }
+  function getLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchPosition);
+  }
+  
   return (
     <div className="col-1">
       <button type="submit" className="btn btn-secondary" onClick={getLocation}>
-        <i class="fas fa-map-marker-alt"> </i>
+        <i className="fas fa-map-marker-alt"> </i>
       </button>
     </div>
   );
--- src/Overview.js
+++ src/Overview.js
@@ -8,7 +8,7 @@ export default function Overview(props) {
     <div className="Overview">
       <h1>{props.cityName},</h1>
       <h2>{props.country}</h2>
-      <div class="text-capitalize"><strong>{props.description}</strong></div>
+      <div className="text-capitalize"><strong>{props.description}</strong></div>
     </div>
   );
 }
